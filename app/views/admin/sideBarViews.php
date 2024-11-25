9a7df179719403e902578bb58eed712e1c77b532
Fitzpatrick Ubay. Managuit <p1trickDev@gmail.com>
2024-11-26 00:40:48 +0800

    admin panel addition

---
+++ app/views/admin/sideBarViews.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Sidebar</title>
+    <link rel="stylesheet" href="/YARNCRAFT/assets/styles/sideBarViewsStyles.css">
+
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="logo">
+            <img src="/E-commerce/assets/images/LOGO.jpg" alt="YarnCraft Emporium Logo">
+        </div>
+        <a href="yarnCraft/app/views/admin/dashboard.php">Dashboard</a>
+        <a href="yarnCraft/app/views/admin/inventory.php">Inventory</a>
+        <a href="yarnCraft/app/views/admin/orders.php">Orders</a>
+        <a href="yarnCraft/app/views/admin/settings.php">Settings</a>
+        <a href="yarnCraft/app/views/admin/users.php">Users</a>
+        <a href="yarnCraft/app/views/admin/reports.php">Reports</a>
+        <a href="yarnCraft/app/views/admin/settings.php">Settings</a>
+        <a href="yarnCraft/app/views/admin/logout.php">Logout</a>
+    </div>
+
+    <script src="/YARNCRAFT/assets/scripts/sideBar.js"></script>
+</body>
+
+</html>
